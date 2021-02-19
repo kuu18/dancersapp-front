7855f32f15ca20f4e8e8c708d5179eda8242c5f8
github user name <kdai18.kuu@gmail.com>
2021-02-19 22:33:05 +0900

    welcome_page_layout

---
+++ components/welcome/welUsage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    welUsage.vue
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ layouts/welcome.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <wel-app-bar />
+    <v-sheet>
+      <v-container
+        fluid
+        :style="{ maxWidth: '1280px' }"
+      >
+        <v-row
+          v-for="(menu, i) in menus"
+          :key="`menu-${i}`"
+        >
+          <v-col cols="12">
+            <div :is="`wel-${menu.title}`" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
+  </v-app>
+</template>
+
+<script>
+import welAbout from '~/components/welcome/welAbout'
+import welUsage from '~/components/welcome/welUsage'
+import WelAppBar from '~/components/welcome/welAppBar'
+
+export default {
+  components: {
+    welAbout,
+    welUsage,
+    WelAppBar
+  },
+  data () {
+    return {
+      menus: [
+        { title: 'about', subtitle: 'このサイトはダンサー向けのアプリケーションです。' },
+        { title: 'usage', subtitle: 'このサイトの使い方' }
+      ]
+    }
+  }
+}
+</script>
