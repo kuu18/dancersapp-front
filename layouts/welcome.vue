fda4b770811e7eedb2a3c5876ac002ca103cfcea
github user name <kdai18.kuu@gmail.com>
2021-02-19 22:50:59 +0900

    add_app_var & eyecatch_image

--- layouts/welcome.vue
+++ layouts/welcome.vue
@@ -1,6 +1,37 @@
 <template>
   <v-app>
-    <wel-app-bar />
+    <wel-app-bar
+      :menus="menus"
+    />
+    <v-img
+      dark
+      :src="image_src"
+      gradient="to top right, rgba(211,211,211,.6), rgba(119,136,153,.9)"
+      :height="imgHeight"
+      max-width="1920"
+      max-height="1080"
+    >
+      <v-row
+        align="center"
+        justify="center"
+        :style="{ height: `${imgHeight}px` }"
+      >
+        <v-col
+          cols="12"
+          class="text-center"
+        >
+          <h1 class="display-1 mb-4">
+            さあ、繋がろうDanceで。
+          </h1>
+          <h4
+            class="subheading"
+            :style="{ letterSpacing: '5px' }"
+          >
+            Danceを中心としたSNSツール
+          </h4>
+        </v-col>
+      </v-row>
+    </v-img>
     <v-sheet>
       <v-container
         fluid
@@ -10,6 +41,18 @@
           v-for="(menu, i) in menus"
           :key="`menu-${i}`"
         >
+          <v-col
+            cols="12"
+          >
+            <v-card flat>
+              <v-card-title class="justify-center display-1">
+                {{ $t(`menus.${menu.title}`) }}
+              </v-card-title>
+              <v-card-text class="text-center">
+                {{ menu.subtitle }}
+              </v-card-text>
+            </v-card>
+          </v-col>
           <v-col cols="12">
             <div :is="`wel-${menu.title}`" />
           </v-col>
@@ -32,6 +75,8 @@ export default {
   },
   data () {
     return {
+      imgHeight: 750,
+      image_src: require('../assets/images/welcome-img.jpg'),
       menus: [
         { title: 'about', subtitle: 'このサイトはダンサー向けのアプリケーションです。' },
         { title: 'usage', subtitle: 'このサイトの使い方' }
