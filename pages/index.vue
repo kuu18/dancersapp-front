6ebd476cd10391e702b4b4084431472f728e102b
github user name <kdai18.kuu@gmail.com>
2021-02-24 18:59:47 +0900

    add_loggedIn_toolbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
     index.vue
-    <wel-app-var />
   </div>
 </template>
 
 <script>
 export default {
   layout ({ store }) {
-    return store.state.loggedIn ? 'default' : 'welcome'
+    return store.state.loggedIn ? 'loggedIn' : 'welcome'
   }
 }
 </script>
