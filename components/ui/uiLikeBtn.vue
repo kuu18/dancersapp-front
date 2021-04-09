2466963430777f4e845c88c79d553f21223dab03
github user name <kdai18.kuu@gmail.com>
2021-04-09 14:35:01 +0900

    add_like_function #32

---
+++ components/ui/uiLikeBtn.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <v-btn
+      v-if="isLiked"
+      color="orange lighten-2"
+      text
+      @click="unlike"
+    >
+      <v-icon>
+        mdi-heart
+      </v-icon>
+    </v-btn>
+    <v-btn
+      v-else
+      color="orange lighten-2"
+      text
+      @click="like"
+    >
+      <v-icon>
+        mdi-heart-outline
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    eventpostId: {
+      type: Number,
+      default: null
+    }
+  },
+  data () {
+    return {
+      likeList: []
+    }
+  },
+  computed: {
+    count () {
+      return this.likeList.length
+    },
+    isLiked () {
+      if (this.likeList.length === 0) { return false }
+      return Boolean(this.findLikeId())
+    }
+  },
+  created () {
+    this.getLikeByPostId().then((response) => {
+      this.likeList = response
+    })
+  },
+  methods: {
+    async getLikeByPostId () {
+      const res = await this.$axios.$get(`/api/v1/likes/?eventpost_id=${this.eventpostId}`)
+      return res
+    },
+    async like () {
+      await this.$axios.$post('/api/v1/likes', { eventpost_id: this.eventpostId })
+      this.getLikeByPostId().then((response) => {
+        this.likeList = response
+      })
+    },
+    async unlike () {
+      await this.$axios.$delete('/api/v1/likes', { data: { eventpost_id: this.eventpostId } })
+      this.getLikeByPostId().then((response) => {
+        this.likeList = response
+      })
+    },
+    findLikeId () {
+      const like = this.likeList.find((like) => {
+        return (like.user_id === this.$auth.user.id)
+      })
+      if (like) { return like.id }
+    }
+  }
+}
+</script>
